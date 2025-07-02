e94949839cb647dc5d593e8d773795a94c01313f
Rupesh Thakur <22rupeshthakur@gmail.com>
2025-07-02 16:40:12 +0000

    facing issue of updating problem the insufficient permission error

---
+++ public/code/3v6nez28QOiqnHdJlr7O/factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+//function to cal the factorial
+int factorial(int num){
+    int fact = 1;
+    for(int i=1;i<=num;i++){
+        fact*=i;
+    }
+    return fact;
+}
+int main(){
+    int fact=1,num=5;
+    printf("factorial of %d is %d",num,factorial(num));
+    
+}
---
+++ public/code/3v6nez28QOiqnHdJlr7O/factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//function to cal the factorial
+int factorial(int num){
+    int fact = 1;
+    for(int i=1;i<=num;i++){
+        fact*=i;
+    }
+    return fact;
+}
+int main(){
+    int fact=1,num=5;
+    cout<<"factorial of"<<num<<" is "<<factorial(num);
+}
