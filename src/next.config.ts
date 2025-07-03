41c6365d618d80d938273586ee777155aed93ff0
Rupesh Thakur <22rupeshthakur@gmail.com>
2025-07-03 14:42:07 +0000

    still the same error facing

---
+++ src/next.config.ts
@@ -0,0 +1,24 @@
+import {withGenkit} from '@genkit-ai/next';
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withGenkit(nextConfig);
